46ff4453884ed31fefd972e8c88d9029066e510e
Ramesh Chuwan <rameshchuwan19@gmail.com>
2019-09-20 18:39:35 -0500

    First jquary commit

---
+++ firstjQuary.js
@@ -0,0 +1,13 @@
+//selecting all divs and giving them a background color of purple
+$("div").css("background", "purple");
+
+//Selecting the divs with the class highlight and making them 200px wide
+$(".highlight").css("width", "200px");
+
+//Selecting the div with the id third and giving the 2px solid orange border
+$("#third").css("border", "2px solid orange");
+
+//Selecting only the first div and changing its font color to pink
+$("div:first-of-type").css("color", "pink");
+//Insted of using "div:first-of-type", we can also use "div:first".s
+
